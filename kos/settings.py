d7cddd8b27ebeba0bbbdf366ee9d8043aaadcc9c
batyacasino <51843016+batyacasino@users.noreply.github.com>
2020-06-28 08:44:40 +0300

    Update settings.py

--- kos/settings.py
+++ kos/settings.py
@@ -1,9 +1,6 @@
 import os
 from django.conf import settings
 
-from django.conf import settings
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
