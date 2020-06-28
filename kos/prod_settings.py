20d2ac7e2423a9aacd882f58dd3819a2b91406a7
batyacasino <51843016+batyacasino@users.noreply.github.com>
2020-06-28 08:45:46 +0300

    Update prod_settings.py

--- kos/prod_settings.py
+++ kos/prod_settings.py
@@ -3,26 +3,25 @@ import os
 from django.conf import settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '$az47@bhkukfu(qn!3cvbc-&edrywns7=9sm5b8jhx9ofy-r#'
+SECRET_KEY = '$az47@bhkukfu(qn!3cvasdbc-&edrywns7=9sm5b8jhx9ofy-r#'
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1']
+
+ALLOWED_HOSTS = ['20.188.56.170', '127.0.0.1']
 
 
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'prompt_db',
-		'USER': 'prompt',
-		'PASSWORD': '929227007Adecc',
+		'NAME': 'promptapp',
+		'USER': 'postgres',
+		'PASSWORD': '929227007Asdec',
 		'HOST': 'localhost',
 		'PORT': '5432',
 	}
 }
 
 
-
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
